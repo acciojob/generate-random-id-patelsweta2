808d1d29493c8d781604a039e1d9467187acd316
patelsweta2 <80040744+patelsweta2@users.noreply.github.com>
2024-04-18 19:19:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function makeid(l) {
   // write your code here
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+	let result = '';
+	for(let i = 0; i<length;i++){
+		const randomIndex = Math.floor(Math.random()*characters.length);
+		result+=characters.charAt(randomIndex);
+	}
+	return result;
 }
 
 // Do not change the code below.
